27b00b473cc0b41111851f8d99f169c3a22ea176
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-03-10 15:37:55 +0600

    add

--- nesting,number,date.sql
+++
@@ -1,5 +0,0 @@
-   select last_name,salary,department_name,countries.country_name,street_address,city
-   from Employees  NATURAL JOIN locations NATURAL JOIN departments  NATURAL JOIN  countries  NATURAL JOIN regions;
-   
-   
-   
