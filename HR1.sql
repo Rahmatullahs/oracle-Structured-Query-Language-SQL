a34a3470aac38f15d27da570b6af474b7e8b063a
Rahmat Ullah <108518708+Rahmatullahs@users.noreply.github.com>
2025-03-17 15:37:59 +0600

    add

--- HR1.sql
+++ HR1.sql
@@ -1,1 +1,35 @@
 --adding constraint syntax
+alter table worker
+add [constraint workers_PK primary key(id)
+]
+
+
+alter table worker
+modify WORKER varchar2(30) not null
+
+
+describe workers;
+
+select constraint_name
+from  user_constraints ;
+
+--modify
+alter table sadier
+MODIFY  WORKER varchar2(30);
+
+
+--rename
+alter table sadier
+rename column last_name to name;
+
+--drop constraint
+alter table sadiar
+drop constraint emp_mgr_pk;
+
+alter table sadiar
+drop primary key;
+
+--dropping data online
+
+--on delete cascade
+enable and deseable
